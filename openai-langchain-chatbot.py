7952f36ceaeafa9ffd2b778e9f968f482b1623d4
jeighmz <48871068+jeighmz@users.noreply.github.com>
2023-10-20 00:49:02 -0400

    added intro messages and input for URL

--- openai-langchain-chatbot.py
+++ openai-langchain-chatbot.py
@@ -6,10 +6,17 @@ from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 
+
+print("Welcome to the Retrieval Augemented Generation (RAG) LangChain Chatbot!")
+print("This chatbot uses the OpenAI API to generate responses to your questions.")
+print("Please enter a URL to a blog post or other text document.")
+print("The chatbot will read the document and use it to answer your questions.")
+
+doc = input("Enter a URL: ")
+
 ## Mainly works with markdown styled web pages (e.g. blog posts). 
 ## web-scraping coming soon...
-
-loader = WebBaseLoader("https://lilianweng.github.io/posts/2022-09-08-ntk/")
+loader = WebBaseLoader(doc)
 data = loader.load()
 
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=0)
@@ -21,6 +28,8 @@ llm = ChatOpenAI()
 retriever = vectorstore.as_retriever()
 memory = ConversationSummaryMemory(llm=llm,memory_key="chat_history",return_messages=True)
 qa = ConversationalRetrievalChain.from_llm(llm, retriever=retriever, memory=memory)
+
+print('Chatbot: Hello, I am a chatbot. Type "exit" to quit.')
 
 while True:
     user_input = input("User: ")
